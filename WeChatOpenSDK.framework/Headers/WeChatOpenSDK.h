be3d4caf644610ad1a49b8e54535c9ffee50e47b
darvin <darvin@tcoding.cn>
2022-03-09 20:49:49 +0800

    微信sdk

---
+++ WeChatOpenSDK.framework/Headers/WeChatOpenSDK.h
@@ -0,0 +1,20 @@
+//
+//  WeChatOpenSDK.h
+//  WeChatOpenSDK
+//
+//  Created by Yuke on 2021/3/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeChatOpenSDK.
+FOUNDATION_EXPORT double WeChatOpenSDKVersionNumber;
+
+//! Project version string for WeChatOpenSDK.
+FOUNDATION_EXPORT const unsigned char WeChatOpenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeChatOpenSDK/PublicHeader.h>
+
+#import "WXApi.h"
+#import "WechatAuthSDK.h"
+#import "WXApiObject.h"
